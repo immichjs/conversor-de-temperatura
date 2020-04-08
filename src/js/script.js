76b9832d686f1e9aa468a2692d5d30a12aaafa0f
Michel França <fr.mich17@gmail.com>
2020-04-08 20:12:15 -0300

    Estilização

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,10 @@ const submit = document.getElementById('btn')
 const fahrenheit = document.getElementById('fahrenheit')
 const kelvin = document.getElementById('kelvin')
 
+celsius.value = 0
+fahrenheit.value = 32
+
 submit.onclick = () => {
     const resultF = (celsius.value * 9/5) + 32
-
     fahrenheit.value = resultF
 }

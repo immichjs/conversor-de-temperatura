6d94d5965beba8fba0df9743051732ba63c5de27
Michel França <fr.mich17@gmail.com>
2020-04-08 23:54:51 -0300

    update in css and js

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,19 @@
+const body = document.getElementById('app')
 const celsius = document.getElementById('celsius')
-const submit = document.getElementById('btn')
 const fahrenheit = document.getElementById('fahrenheit')
-const kelvin = document.getElementById('kelvin')
+const submit = document.getElementById('btn')
 
 celsius.value = 0
-fahrenheit.value = 32
+fahrenheit.value = `${32} °F`
 
 submit.onclick = () => {
     const resultF = (celsius.value * 9/5) + 32
-    fahrenheit.value = resultF
+    !isNaN(resultF) ? fahrenheit.value = `${resultF} °F` : fahrenheit.value = 'Valor inválido'
 }
+
+body.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault()
+        submit.click()
+    }
+})

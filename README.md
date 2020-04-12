873bd517368eeb5e643fe0844f6d5ac9b07da9d4
Michel França <fr.mich17@gmail.com>
2020-04-12 01:09:06 -0300

    Mais correções de bugs

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
-![Image description](https://i.imgur.com/m6x2NO9.png)
+# Conversor de Temperatura
 
-- No momento só converte Celsius para Fahrenheit, mas em breve outros tipos serão implementados.
+Eu estava sem ideia do que desenvolver, então resolvi um projeto simples, que basicamente converte temperatura de graus celsius (C) para grau fahrenheit (F).
 
-## Features
+![Image description](https://i.imgur.com/Elk47Xc.png)
 
-[x] Conversor de fahrenheit. <br/>
-[x] Desabilitar o botão de converter quando iniciar a aplicação.<br/>
-[x] Icon.<br/> 
-![Image description](https://i.imgur.com/Z9kxVgf.png) <br/><br/>
-[] Adicionar um conversor pra Kelvin.<br/>
-[] Adicionar botões pra fahrenheit, kelvin ou celsius de acordo com o usuário quer.<br/>
-[] Simular um pop-up de resultado.<br/><br/>
+## Começando
 
+Na primeica caixa de texto é inserido o valor númerico para ser convertido.
+
+![Image description](https://i.imgur.com/roRAInQ.png)
+
+Na segunda caixa de texto após clicar em "Converter" ou pressionar "Enter" será exibido o grau celsius convertido para Fahrenheit. Também é possível copiar o mesmo através do botão ao lado direito da segunda caixa de texto.
+
+![Image description](https://i.imgur.com/3GkPCZV.png)
+
+
+## Feito utilizando
+
+- HTML5
+- CSS3
+- Javascript
+- Flaticon
+
+## Author
+
+* **Michel França** - [immichjs](https://github.com/immichjs)
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,57 @@
-const body = document.getElementById('app')
+const app = document.getElementById('app')
+
 const celsius = document.getElementById('celsius')
 const fahrenheit = document.getElementById('fahrenheit')
 const submit = document.getElementById('btn')
+
 const clipboard = document.getElementById('clipboardButton')
 const clipboardLegend = document.getElementById('clipboardLegend')
 
-celsius.value = 0
-fahrenheit.value = `${32} °F`
-submit.value = 'Converter'
+const placeholder = (input, value) => input.setAttribute('placeholder', value)
+
+function graus (celsiusText, fahrenheitText) {
+    placeholder(celsius, celsiusText)
+    placeholder(fahrenheit, fahrenheitText)
+}
+
+graus('Grau Celsius ( °C )', 'Grau Fahrenheit ( °F )')
 
 celsius.onclick = () => {
-    fahrenheit.value = ''
-    celsius.value = ''
+    graus('', '')
+    if (fahrenheit.value)
+        return
 }
 
 submit.onclick = () => {
+    const celsiusValue = celsius.value.toString()
     const resultF = (celsius.value * 9/5) + 32
-    if (celsius.value === '' || isNaN(resultF)) {
-            fahrenheit.value = 'Valor inválido'
-            celsius.value = 'Valor obrigatório' 
-    } else {
+    celsius.value == '' || isNaN(celsius.value) || !isFinite(resultF) || celsiusValue.includes('e') ?
+        fahrenheit.value = 'Valor acima é inválido ou nulo' :
         fahrenheit.value = `${resultF} °F`
+}
+
+app.onkeyup = (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        submit.click()
     }
 }
 
-body.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        e.preventDefault()
-        submit.click()
-    }
-})
-
 clipboard.onclick = () => {
-    fahrenheit.select()
-    document.execCommand('copy')
-    clipboardLegend.innerHTML = 'Copiado'
+    if (fahrenheit.value) {
+        fahrenheit.select()
+        document.execCommand('copy')
+        clipboardLegend.innerHTML = 'Copiado'
+        clipboardLegend.style.color = '#7bbb44'
+    } else {
+        clipboardLegend.innerHTML = 'Não copiado'
+        clipboardLegend.style.color = '#f14235'
+    }
 }
 
 clipboard.onmouseout = () => {
     setTimeout(() => {
         clipboardLegend.innerHTML = 'Copiar'
+        clipboardLegend.style.color = '#fff'
     }, 250)
 }
